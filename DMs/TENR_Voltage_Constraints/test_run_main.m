5ef3475469906de1a09721165bc8dea19532fb77
Mazhar Ali <mazhar.ali@skolkovotech.ru>
2021-08-24 22:25:10 +0300

    Update test_run_main.m

--- DMs/TENR_Voltage_Constraints/test_run_main.m
+++ DMs/TENR_Voltage_Constraints/test_run_main.m
@@ -5,13 +5,13 @@ clearvars;
 
 %% Initialization Block ........................................................
 
-data       = case300; 
+data       = case14; 
 testcase   = ext2int( data );                                              % Change the test case ..........................
 n_stepx    = 0.5;                                                         % Newton step size alpha,   0 < alpha <= 1,  normally a small value is selected......
 
 %% Scenario for test case (Please "Bus_fstage" values shouldn't exceed from the number of total buses in the network.)
 
-Bus_fstage = [ 1:1:300 ];                                                   % Buses, where loadability \lambda is subjected .............    
+Bus_fstage = [ 1:1:14 ];                                                   % Buses, where loadability \lambda is subjected .............    
 a_bus_p    = 1.0;                                                          % On/off status of active power injection "1" ON and "0" is OFF ................................
 b_bus_q    = 1.0;                                                          % On/off status of reactive power injection "1" ON and "0" is OFF ................
 
@@ -20,26 +20,9 @@ Vmin       = 0.90;                                                         % min
 
 %% running TENR algorithm ..........................
 
-s_tole = 0;                                                             
-count_l = 0;
-
-while 1
-
 step = randsample(n_stepx, 1);                                             % random selection of optimal step size \alpha for global convergence ......................
 
 [Vx, Vy, Slack_H, Slack_L, l, Tole, Iter, tnr, Stabi, Jx ] = tnr_testvol(testcase,step,Bus_fstage,a_bus_p,b_bus_q,Vmax,Vmin);           % Lambda is maximum Loadability, Tole is tolerance and Iter is the total iterations ........ 
-
-count_l = count_l+1;
-s_tole = Tole;
-
-%% closing while loop ..............................
-
-if s_tole > 1e-3
-    continue
-else
-    break
-end
-end
 
 l
 Tole
